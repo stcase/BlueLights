e1a44d76c51aad7eda11ff1f0280c4a2b721d365
Steven Case <stcinc@gmail.com>
2021-05-12 23:44:09 +0200

    Don't count brief bluetooth spikes as trigger
    Occasionally the RSSI of a signal will jump unusually high for one
    or two measurements. This will look at the signal over a longer
    period to prevent those abnormal spikes from triggering the lights.

--- bluelights/bluetooth.py
+++ bluelights/bluetooth.py
@@ -4,7 +4,7 @@ from platform import system
 from queue import Queue
 from threading import Thread
 from time import sleep
-from typing import Callable
+from typing import Callable, Dict, List
 
 if system() == "Linux":
     from bluepy.btle import DefaultDelegate, ScanEntry, Scanner
@@ -51,11 +51,42 @@ class BluetoothProximity(Thread):
 
 
 class BluetoothProximityHandler(DefaultDelegate):
-    def __init__(self, callback: Callable[[str, int], None], threshold_rssi: int) -> None:
+    def __init__(self, callback: Callable[[str, int], None], threshold_rssi: int, threshold_timer: float = 2) -> None:
+        """
+        Args:
+            callback: Function to be called when a device is seen
+            threshold_rssi: The threshold rssi for a seen device to trigger the light
+            threshold_timer: The duration a light must be above threshold for to trigger the light
+        """
         self._threshold_rssi = threshold_rssi
+        self._threshold_timer = threshold_timer
         self._callback = callback
+        self._seen_devices: Dict[str, List[float]] = {}  # _seen_devices[addr] = [last seen, most recent]
+
+    def _passes_threshold(self, dev: ScanEntry) -> bool:
+        """
+        Determines whether a device should trigger the light or not by seeing how long the device has been above the
+        threshold. Also maintains the `_seen_devices` dictionary.
+
+        Returns:
+            True if the light should be triggered by this device
+        """
+        triggered = False
+        now = datetime.now().timestamp()
+        if int(dev.rssi) >= self._threshold_rssi:
+            timediff = now - self._seen_devices.setdefault(dev.addr, [now, now])[0]
+            self._seen_devices[dev.addr][1] = now
+            triggered = triggered or timediff >= self._threshold_timer
+        elif dev.addr in self._seen_devices:
+            del self._seen_devices[dev.addr]
+
+        for addr, times in list(self._seen_devices.items()):
+            if now - times[1] > 2 * self._threshold_timer:
+                del self._seen_devices[addr]
+
+        return triggered
 
     def handleDiscovery(self, dev: ScanEntry, isNewDev: bool, isNewData: bool) -> None:
         logging.debug(f"Seen device {dev.addr}, strength: {dev.rssi}")
-        if int(dev.rssi) >= self._threshold_rssi:
+        if self._passes_threshold(dev):
             self._callback(dev.addr, dev.rssi)
--- tests/test_bluetooth.py
+++ tests/test_bluetooth.py
@@ -20,19 +20,41 @@ class TestBluetoothProximity:
         assert actual == LightSwitch(datetime(2020, 1, 1, 12, 0, 5).timestamp(), 50, Color(1, 2, 3))
 
 
+@freeze_time("2020-01-01 12:00:02")
 class TestBluetoothProximityHandler:
     class MockScanEntry:
         rssi = -30
         addr = "fa:ke:ad:dr"
 
-    def test_in_range(self) -> None:
+    def test_callback_called(self) -> None:
         mock = Mock()
         handler = BluetoothProximityHandler(mock.callback, -50)
+        handler._seen_devices[self.MockScanEntry.addr] = [0, 0]
         handler.handleDiscovery(self.MockScanEntry(), False, False)
         mock.callback.assert_called_once_with("fa:ke:ad:dr", -30)
 
-    def test_not_in_range(self) -> None:
+    def test_callback_not_called(self) -> None:
         mock = Mock()
         handler = BluetoothProximityHandler(mock.callback, -25)
+        time = datetime.now().timestamp()
+        handler._seen_devices[self.MockScanEntry.addr] = [time, time]
         handler.handleDiscovery(self.MockScanEntry(), False, False)
         mock.callback.assert_not_called()
+
+    def test_passes_threshold_addr_added(self) -> None:
+        mock = Mock()
+        handler = BluetoothProximityHandler(mock.callback, -40)
+        now = datetime.now().timestamp()
+        assert not handler._passes_threshold(self.MockScanEntry())
+        assert handler._seen_devices == {self.MockScanEntry.addr: [now, now]}
+
+    def test_passes_threshold_removes_old(self) -> None:
+        old_addr = "ol:dd:ad:dr"
+        new_addr = "ne:ww:ad:dr"
+        mock = Mock()
+        handler = BluetoothProximityHandler(mock.callback, -25)
+        now = datetime.now().timestamp()
+        handler._seen_devices = {old_addr: [0, 0], new_addr: [now, now]}
+        handler._passes_threshold(self.MockScanEntry())
+        assert new_addr in handler._seen_devices
+        assert old_addr not in handler._seen_devices
